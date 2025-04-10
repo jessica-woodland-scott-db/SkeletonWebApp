179aa179e5d7a7c869a67867c82a99580f1d1a24
JWoodland-Scott <messica@hotmail.co.uk>
2025-04-10 14:21:21 +0100

    adding henry

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,6 +6,10 @@ public class QueryProcessor {
 
     System.out.println("Received query:" + query);
 
+    if (query.equals("What is your name?")) {
+      return "Jess";
+    }
+
     if (query.toLowerCase().contains("shakespeare")) {
       return "William Shakespeare (26 April 1564 - 23 April 1616) was an "
           + "English poet, playwright, and actor, widely regarded as the greatest "
@@ -13,7 +17,7 @@ public class QueryProcessor {
     }
 
     if (query.toLowerCase().contains("henry")) {
-      return "enry VIII (28 June 1491 – 28 January 1547) was King of England from 22 April " +
+      return "Henry VIII (28 June 1491 – 28 January 1547) was King of England from 22 April " +
               "1509 until his death in 1547. Henry is known for his six marriages and his efforts " +
               "to have his first marriage (to Catherine of Aragon) annulled. His disagreement with " +
               "Pope Clement VII about such an annulment led Henry to initiate the English Reformation, " +
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -20,4 +20,9 @@ public class QueryProcessorTest {
     assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
   }
 
+  @Test
+  public void knowsWhatIsYourName() throws Exception {
+    assertThat(queryProcessor.process("What is your name?"), containsString("Jess"));
+  }
+
 }
